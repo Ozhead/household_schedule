1c9f69efba35edb861993a624554610202db72a3
Ozhead <qh6ma@web.de>
2021-10-15 19:08:33 +0200

    Providing tests for Task class

---
+++ tests/test_task.py
@@ -0,0 +1,19 @@
+from task import Task
+from meta_task import MetaTask, ElementNotMetaTaskException
+from task_factory import TaskFactory
+import pytest
+
+
+def test_task():
+    mt = MetaTask("A", 5)
+    t = TaskFactory.create_task(mt)
+
+    assert type(t) == Task
+    assert t.get_name() == "A"
+    assert t.get_deadline() == 5
+    assert t.get_origin() == mt
+
+
+def test_task_errors():
+    with pytest.raises(ElementNotMetaTaskException):
+        TaskFactory.create_task(5)
