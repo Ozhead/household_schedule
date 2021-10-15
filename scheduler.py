627690bdc26e2dfd45cb67a79ef64fc8ce20c9e4
Ozhead <qh6ma@web.de>
2021-10-15 19:15:20 +0200

    Providing test for Scheduler.update_queue method

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,5 @@
 from meta_task import MetaTask, ElementNotMetaTaskException
+from task import Task
 from typing import List
 
 
@@ -10,7 +11,7 @@ class Scheduler:
 
         self._tasks: List[MetaTask] = meta_tasks
         self._day_ctr: int = 0
-        self._queue = []
+        self._queue: List[Task] = []
 
     # getter/setter
     def get_tasks(self) -> List[MetaTask]:
@@ -19,5 +20,11 @@ class Scheduler:
     def get_day_ctr(self) -> int:
         return self._day_ctr
 
+    def get_queue(self) -> List[Task]:
+        return self._queue
+
     def incr_day_ctr(self):
         self._day_ctr = self._day_ctr + 1
+
+    def update_queue(self):
+        pass
--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,6 +1,7 @@
 import pytest
 from scheduler import Scheduler, ElementNotMetaTaskException
 from meta_task import MetaTask
+from task_factory import TaskFactory
 
 
 def test_scheduler_errors():
@@ -14,3 +15,15 @@ def test_scheduler_ctor():
     s = Scheduler([a, b])
     assert s.get_day_ctr() == 0
     assert s.get_tasks() == [a, b]
+
+
+def test_scheduler_update_schedule():
+    a = MetaTask("A", 1)
+    b = MetaTask("B", 5)
+    s = Scheduler([a, b])
+    ta = TaskFactory.create_task(a)
+    tb = TaskFactory.create_task(b)
+
+    assert s.get_queue() == []
+    s.update_queue()
+    assert s.get_queue() == [ta, tb]
